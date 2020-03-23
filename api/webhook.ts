9a7fdf6fc18cfe4169008decfce1f4200aba9179
Joe Porpeglia <josephp@spotify.com>
2020-03-22 20:38:53 -0400

    return bot info directly

--- api/webhook.ts
+++ api/webhook.ts
@@ -5,6 +5,5 @@ const { telegram } = new Telegraf(process.env.BOT_TOKEN as string);
 
 export default async (req: NowRequest, res: NowResponse) => {
   const me = await telegram.getMe();
-  console.log(JSON.stringify(me, null, 2));
-  res.status(200);
+  res.json(me);
 };
