d2bc6e1d429afa816f470fb7072d4aac13114de9
Joe Porpeglia <josephp@spotify.com>
2020-03-23 01:40:37 -0400

    Add list endpoint

--- api/webhook.ts
+++ api/webhook.ts
@@ -1,36 +1,56 @@
 import { NowRequest, NowResponse } from "@now/node";
+import fetch from "node-fetch";
 import { Update, Message, User } from "telegraf/typings/telegram-types";
 import * as airtable from "airtable";
 
 type PointsRecord = {
-  chat_id: string;
-  user_id: string;
+  chat_id: number;
+  user_id: number;
   points: number;
+};
+
+type TelegramActionResponse = {
+  method: string;
+  chat_id: number;
+  reply_to_message_id?: number;
+  text: string;
 };
 
 export default async (req: NowRequest, res: NowResponse) => {
   const update: Update = req.body;
   const message = update.message;
 
-  if (!message?.text?.startsWith("@pointz_bot ")) {
-    return res.status(200).send("ok");
+  if (!message) {
+    throw new Error("Update received with no message");
   }
 
   const response = await handleMessage(message);
-  res.setHeader("Content-Type", "application/json");
-  res.send(JSON.stringify(response));
+
+  // Return any response to telegram since they may include actions.
+  if (response) {
+    res.setHeader("Content-Type", "application/json");
+    res.send(JSON.stringify(response));
+    return;
+  }
+
+  res.status(200).send("");
 };
 
-async function handleMessage(message: Message) {
+async function handleMessage(
+  message: Message
+): Promise<TelegramActionResponse | void> {
+  // Ignore everything except mentions
+  if (!message.text?.startsWith("@pointz_bot ")) {
+    return;
+  }
+
   if (shouldAssignPoints(message)) {
     return handleAssignPoints(message);
   }
 
-  //   if (shouldListPoints(message)) {
-  //     return handleListPoints(message);
-  //   }
-
-  return;
+  if (shouldListPoints(message)) {
+    return handleListPoints(message);
+  }
 }
 
 function shouldAssignPoints(message: Message) {
@@ -40,36 +60,42 @@ function shouldAssignPoints(message: Message) {
   );
 }
 
+function shouldListPoints(message: Message) {
+  return (
+    message.reply_to_message == null && message.text === "@pointz_bot list"
+  );
+}
+
 function parsePointAmount(text: string) {
   return parseInt(text.replace("@pointz_bot ", ""), 10);
 }
 
-async function handleAssignPoints(message: Message) {
+async function handleAssignPoints(
+  message: Message
+): Promise<TelegramActionResponse> {
   const sender = message.from;
   const recipient = message.reply_to_message?.from;
   const text = message.text ?? "";
   const amount = parsePointAmount(text) || 0;
 
+  let response;
+
   if (sender?.id === recipient?.id && amount > 0) {
-    return `You can't give points to yourself lol.`;
+    response = `You can't give points to yourself lol.`;
+  } else if (Math.abs(amount) > 10) {
+    response = "Nope!";
+  } else {
+    const record = await assignPointsToUser(
+      message.chat.id,
+      recipient?.id as number,
+      amount
+    );
+
+    const totalPoints = record.get("points");
+    const pointLabel = totalPoints === 1 ? "point" : "points";
+
+    response = `${getDisplayName(recipient)} has ${totalPoints} ${pointLabel}!`;
   }
-
-  if (Math.abs(amount) > 10) {
-    return "Nope!";
-  }
-
-  const record = await assignPointsToUser(
-    String(message.chat.id),
-    String(recipient?.id),
-    amount
-  );
-
-  const totalPoints = record.get("points");
-  const pointLabel = totalPoints === 1 ? "point" : "points";
-
-  const response = `${getDisplayName(
-    recipient
-  )} has ${totalPoints} ${pointLabel}!`;
 
   return {
     method: "sendMessage",
@@ -87,15 +113,35 @@ function getDisplayName(user?: User) {
   return user.first_name ?? user.username;
 }
 
+async function handleListPoints(
+  message: Message
+): Promise<TelegramActionResponse> {
+  const topPoints = await getTopThreePoints(message.chat.id);
+  const topUsers = await Promise.all(
+    topPoints.map(r => getUser(r.get("chat_id"), r.get("user_id")))
+  );
+
+  return {
+    method: "sendMessage",
+    chat_id: message.chat.id,
+    text: topUsers
+      .map(
+        (u, i) =>
+          `${i + 1}. ${getDisplayName(u)} - ${topPoints[i].get("points")}`
+      )
+      .join("\n")
+  };
+}
+
 const table = airtable.base("app3AKqySx0bYlNue");
 const pointsTable = table<PointsRecord>("points");
 
 async function assignPointsToUser(
-  chatId: string,
-  userId: string,
+  chatId: number,
+  userId: number,
   points: number
 ): Promise<airtable.Record<PointsRecord>> {
-  const record = await getPointsRecord(chatId, userId);
+  const record = await getPointsRecordForUser(chatId, userId);
   const newPoints = record.get("points") + points;
   record.set("points", newPoints);
 
@@ -110,9 +156,29 @@ async function assignPointsToUser(
   );
 }
 
-async function getPointsRecord(
-  chatId: string,
-  userId: string,
+async function getTopThreePoints(
+  chatId: number
+): Promise<airtable.Record<PointsRecord>[]> {
+  return new Promise((res, rej) => {
+    pointsTable
+      .select({
+        filterByFormula: `{chat_id} = ${chatId}`,
+        pageSize: 3,
+        sort: [{ field: "points", direction: "desc" }]
+      })
+      .firstPage(async (err, records) => {
+        if (err) {
+          return rej(err);
+        }
+
+        res(records);
+      });
+  });
+}
+
+async function getPointsRecordForUser(
+  chatId: number,
+  userId: number,
   initialPoints: number = 0
 ): Promise<airtable.Record<PointsRecord>> {
   return new Promise((res, rej) => {
@@ -134,8 +200,8 @@ async function getPointsRecord(
 }
 
 async function createPointsRecord(
-  chatId: string,
-  userId: string,
+  chatId: number,
+  userId: number,
   initialPoints: number
 ): Promise<airtable.Record<PointsRecord>> {
   return new Promise((res, rej) => {
@@ -158,4 +224,18 @@ async function createPointsRecord(
       }
     );
   });
+}
+
+async function getUser(
+  chatId: number,
+  userId: number
+): Promise<User | undefined> {
+  const params = new URLSearchParams();
+  params.set("chat_id", String(chatId));
+  params.set("user_id", String(userId));
+  const response = await fetch(
+    `https://api.telegram.org/bot${process.env.BOT_TOKEN}/getChatMember?${params}`
+  );
+  const { result } = await response.json();
+  return result;
 }
