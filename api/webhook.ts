c62caa83f31d2a6065f9421ce8169eedbc3a77ff
Joe Porpeglia <josephp@spotify.com>
2020-03-22 20:29:19 -0400

    Add webhook

---
+++ api/webhook.ts
@@ -0,0 +1,9 @@
+import { NowRequest, NowResponse } from "@now/node";
+import Telegraf from "telegraf";
+
+const { telegram } = new Telegraf(process.env.BOT_TOKEN as string);
+
+export default async (req: NowRequest, res: NowResponse) => {
+  const me = await telegram.getMe();
+  console.log(JSON.stringify(me, null, 2));
+};
