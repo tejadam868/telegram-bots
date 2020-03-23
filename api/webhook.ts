cd568dfb65e6a364ed84fc0752fddcfae84f2436
Joe Porpeglia <josephp@spotify.com>
2020-03-22 20:34:14 -0400

    Change to trigger deploy

--- api/webhook.ts
+++ api/webhook.ts
@@ -6,4 +6,5 @@ const { telegram } = new Telegraf(process.env.BOT_TOKEN as string);
 export default async (req: NowRequest, res: NowResponse) => {
   const me = await telegram.getMe();
   console.log(JSON.stringify(me, null, 2));
+  res.status(200);
 };
