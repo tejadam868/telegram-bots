1ae913326dd891ae5353c71318d24dedb7757597
Joe Porpeglia <josephp@spotify.com>
2020-03-22 21:31:40 -0400

    Log useful values

--- api/webhook.ts
+++ api/webhook.ts
@@ -1,9 +1,20 @@
 import { NowRequest, NowResponse } from "@now/node";
 import Telegraf from "telegraf";
+import { Update } from "telegraf/typings/telegram-types";
 
 const { telegram } = new Telegraf(process.env.BOT_TOKEN as string);
 
 export default async (req: NowRequest, res: NowResponse) => {
-  console.log(req.body);
+  const update: Update = req.body;
+  const chatId = update.message?.chat.id;
+  const inputPoints = update.message?.text;
+  const sender = update?.message?.from;
+  const recipient = update?.message?.reply_to_message?.from;
+
+  console.log("chatId", chatId);
+  console.log("inputPoints", inputPoints);
+  console.log("sender", sender);
+  console.log("recipient", recipient);
+
   res.status(200).send("ok");
 };
